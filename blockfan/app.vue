6c4479402f71da99ef148bed7cee974e8140f6f9
Chris.Fan <fanchi0917@gmail.com>
2023-12-11 00:07:59 +0800

    [homepage] edit list style

--- blockfan/app.vue
+++ blockfan/app.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <img src="/letter-f.png" alt="">
     <h1>Chris Fan</h1>
+    <h2>Front End Develper</h2>
+    <h3>fanchi0917@gmail.com</h3>
+    <ul>
+      <li>
+        <a href="https://twitter.com/fanchi0917" target="_blank">Twitter</a>
+      </li>
+      <li>
+        <a href="https://twitter.com/all_weather_p" target="_blank">All Weather Protocol Twitter</a>
+      </li>
+    </ul>
   </div>
 </template>
--- blockfan/nuxt.config.ts
+++ blockfan/nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  ssr: false
+  ssr: false,
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/letter-f.png' },
+      ],
+      title: "Chris Fan"
+    },
+  },
+  css: [
+    'assets/main.css',
+  ],
 })
